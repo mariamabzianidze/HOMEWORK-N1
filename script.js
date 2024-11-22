051b0a2e64c6b86e4eda491304c6da65a4a9f6b0
Mariam Abzianidze <abzianidz3@gmail.com>
2024-11-22 23:39:07 +0400

    update

--- script.js
+++
@@ -1,4 +0,0 @@
-let fullName = prompt("Enter your name:");
-let birthYear = prompt("Enter your birth year:");
-let address = prompt("Enter your address:");
-alert(`გამარჯობა ${fullName} მიხარია რომ უკვე ${2024-Number(birthYear)} წლის ხარ და ${address} აქ ცხოვრობ.`);
